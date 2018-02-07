c6aac5e9d43b2de91ab8ce4a054d57f53bad23ce
khuong291 <dkhuong291@gmail.com>
2018-02-07 20:15:05 +0700

    Initial commit

---
+++ Binder/Binder.h
@@ -0,0 +1,19 @@
+//
+//  Binder.h
+//  Binder
+//
+//  Created by KhuongPham on 2/7/18.
+//  Copyright © 2018 KhuongPham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Binder.
+FOUNDATION_EXPORT double BinderVersionNumber;
+
+//! Project version string for Binder.
+FOUNDATION_EXPORT const unsigned char BinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Binder/PublicHeader.h>
+
+
